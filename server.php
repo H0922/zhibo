02b361c38ef5593d88510b34bfa470179d152627
“B0514” <737051678@qq.com>
2020-02-26 12:36:20 +0000

    addd

--- server.php
+++ server.php
@@ -13,12 +13,36 @@ $ws->on('message', function ($ws, $frame) {
     // var_dump($frame);
     $info = json_decode($frame->data,true);
     if($info['type']=='login'){
+        $redis=new Swoole\Coroutine\Redis();
+        $key="online_list";
+        $redis->connect('127.0.0.1',6379);
+        $list=$redis->get($key);
+        $userlist=json_decode($list,true);
+        if(empty($userlist)){
+            $userlist=[];
+        }
+        $userlist[]=[
+            'client_id'=>$frame->fd,
+            'username'=>$info['con']
+        ];
+        $str=json_encode($userlist,JSON_UNESCAPED_UNICODE);
+        $redis->set($key,$str);
         $message=[
             'is_me'=>1,
             'username'=>$info['con']
         ];
-        $res=json_encode($message);
+        $res=json_encode($message,JSON_UNESCAPED_UNICODE);
         $ws->push($frame->fd, $res);
+        foreach($userlist as $key=>$val){
+            if($frame->fd == $val['client_id']){
+                $message=[
+                    'is_me'=>0,
+                    'username'=>$info['con']
+                ];
+                $res=json_encode($message,JSON_UNESCAPED_UNICODE);
+                $ws->push($frame->fd, $res);
+            }
+        }
     }
  
 });
